4961d70e5549ebbeec870266689b3a44dfe6a0f2
wowanamatsu <antonioflavionte@gmail.com>
2017-06-13 23:43:59 -0300

    Ajustes no sistema de usuários. Usando devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  enum role: [:normal, :convidado, :admin, :staff]
+  enum status: [:inativo, :ativo]
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable,  :registerable,
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name, message: 'deve ser preenchido.'
+
+  validate :password_complexity
+  def password_complexity
+    if password.present?
+      if !password.match(/^(?=.*\d)(?=.*\W)(?=.*([a-z]))(?=.*([A-Z]))([\x20-\x7E]){8,40}$/) 
+        errors.add :password, "Senha muito fraca!"
+      end
+    end
+  end
 end
