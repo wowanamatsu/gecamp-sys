83beda177b1a11bdd536b4eaa597761876c4668a
wowanamatsu <antonioflavionte@gmail.com>
2017-06-19 18:54:26 -0300

    Ajustes no cadastro de Pessoas: mascaras, buscas..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require estados
 //= require rails-ujs
 //= require notify.min
+//= require validacoes
--- app/controllers/pessoas_controller.rb
+++ app/controllers/pessoas_controller.rb
@@ -4,7 +4,32 @@ class PessoasController < ApplicationController
   # GET /pessoas
   # GET /pessoas.json
   def index
-    @pessoas = Pessoa.all
+    if params[:select2_trigger]
+      if params[:q]
+        @pessoas = Pessoa.select("pessoas.id, pessoas.nome")
+        .where("(TRANSLATE(lower(pessoas.nome), 
+          'áéíóúàèìòùãõâêîôôäëïöüçÁÉÍÓÚÀÈÌÒÙÃÕÂÊÎÔÛÄËÏÖÜÇ', 
+          'aeiouaeiouaoaeiooaeioucAEIOUAEIOUAOAEIOOAEIOUC') 
+          like '%#{params[:q].downcase}%' or lower(pessoas.nome) 
+          like '%#{params[:q].downcase}%')")
+        @pessoa = @pessoas.page(params[:page] || 1).per(10)
+      end
+
+      respond_to do |format|
+        format.html
+        format.json {
+          render :json => {
+            :pessoas => @pessoa,
+            :total => @pessoa.count,
+            :links => { :self => @pessoa.current_page, :next => @pessoa.next_page}
+          }
+        }
+      end
+    else
+      @pessoas = Pessoa.select(:id, :nome, :endereco, :telefone_residencial, :celular)
+      .order(:nome).page(params[:page] || 1).per(10)
+      render action: :index, layout: request.xhr? == nil
+    end
   end
 
   # GET /pessoas/1
@@ -68,4 +93,4 @@ class PessoasController < ApplicationController
     def pessoa_params
       params.require(:pessoa).permit(:nome, :endereco, :cidade_id, :bairro_id, :cep, :telefone_residencial, :celular, :email, :estado_civil, :sexo, :cor, :renda_familiar, :numero_de_filhos, :numero_de_dependentes, :user_id, :pessoa_id, :observacoes, :estado_id, :municipio_id, :data_nascimento)
     end
-end
+  end
