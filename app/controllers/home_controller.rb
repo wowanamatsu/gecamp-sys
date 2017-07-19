f4eea6527585b5178f6b563bbd596492dcde6c59
wowanamatsu <antonioflavionte@gmail.com>
2017-07-18 22:19:43 -0300

    Exibição dos aniversariantes do mês e do dia corrente.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @pessoas = Pessoa
+               .select(:nome, :data_nascimento, :celular)
+               .where("EXTRACT(DAY FROM data_nascimento) = #{Time.now.strftime('%d').to_i}")
+               .where("EXTRACT(MONTH FROM data_nascimento) = #{Time.now.strftime('%m').to_i}")
+    @aniv_mes = Pessoa.select(:id)
+                .where("EXTRACT(MONTH FROM data_nascimento) = #{Time.now.strftime('%m').to_i}")
+    @total = @aniv_mes.count
   end
 end
