e80485bc5e451e0bf72aa3362907189beb8d2e74
wowanamatsu <antonioflavionte@gmail.com>
2017-08-16 21:20:54 -0300

    Ajustes: adicioando select2 no campo de indicados por para geração de PDFs.

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -1,8 +1,9 @@
 class RelatoriosController < ApplicationController
 	
 	def index
-		if params[:indicado_por] and params[:indicado_por] != ''
-			@people = Pessoa.where("pessoa_id = ?", params[:indicado_por])
+		if params[:pessoa]['indicado_por'] and params[:pessoa]['indicado_por'] != ''
+			# @people = Pessoa.where("pessoa_id = ?", params[:indicado_por])
+			@people = Pessoa.where("pessoa_id = ?", params[:pessoa]['indicado_por'])
 			@result_indicado_por = "foram encontrados #{@people.count}, registros no sistema."
 			@display_indicado_por = @people.count > 0 ? true : false
 		end
@@ -12,8 +13,8 @@ class RelatoriosController < ApplicationController
 			format.json { render :json => @people }
 
 			format.pdf do
-				if params[:indicado_por] and params[:indicado_por] != ''
-					pdf = IndicadosPDF.new(:args => params[:indicado_por], :user => current_user)
+				if params[:pessoa]['indicado_por'] and params[:pessoa]['indicado_por'] != ''
+					pdf = IndicadosPDF.new(:args => params[:pessoa]['indicado_por'], :user => current_user)
 					send_data pdf.render, :filename => "relatorios_#{@people}.pdf", :type => "application/pdf", :disposition => "inline"
 				end
 			end
