c13e3ce132594b3d55d43316963bd76d992b9f29
wowanamatsu <antonioflavionte@gmail.com>
2017-09-10 12:25:59 -0300

    ajustes iniciais no cadastro de carros.

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -1,10 +1,38 @@
 class CarrosController < ApplicationController
   before_action :set_carro, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /carros
   # GET /carros.json
   def index
-    @carros = Carro.all
+    if params[:select2_trigger]
+      if params[:q]
+        @carros = Carro.select(:id, :nome, :pessoa_id, :placa, :cor)
+        .where("(TRANSLATE(lower(carros.nome), 
+          'áéíóúàèìòùãõâêîôôäëïöüçÁÉÍÓÚÀÈÌÒÙÃÕÂÊÎÔÛÄËÏÖÜÇ', 
+          'aeiouaeiouaoaeiooaeioucAEIOUAEIOUAOAEIOOAEIOUC') 
+          like '%#{params[:q].downcase}%' or lower(carros.nome) 
+          like '%#{params[:q].downcase}%')")
+
+        @carro = @carros.page(params[:page] || 1).per(10)
+      end
+
+      respond_to do |format|
+        format.html
+        format.json {
+          render :json => {
+            :carros => @carro,
+            :total => @carro.count,
+            :links => { :self => @carro.current_page, :next => @carro.next_page}
+          }
+        }
+      end
+
+    else
+      @carros = Carro.select(:id, :nome, :pessoa_id, :cor, :placa, :observacoes,)
+      .order(nome: :desc).page(params[:page] || 1).per(8)
+      render action: :index, layout: request.xhr? == nil
+    end
   end
 
   # GET /carros/1
@@ -29,7 +57,7 @@ class CarrosController < ApplicationController
 
     respond_to do |format|
       if @carro.save
-        format.html { redirect_to @carro, notice: 'Carro was successfully created.' }
+        format.html { redirect_to @carro, notice: 'Carro criado com sucesso.' }
         format.json { render :show, status: :created, location: @carro }
       else
         format.html { render :new }
@@ -43,7 +71,7 @@ class CarrosController < ApplicationController
   def update
     respond_to do |format|
       if @carro.update(carro_params)
-        format.html { redirect_to @carro, notice: 'Carro was successfully updated.' }
+        format.html { redirect_to @carro, notice: 'Carro atualizado com sucesso.' }
         format.json { render :show, status: :ok, location: @carro }
       else
         format.html { render :edit }
@@ -57,7 +85,7 @@ class CarrosController < ApplicationController
   def destroy
     @carro.destroy
     respond_to do |format|
-      format.html { redirect_to carros_url, notice: 'Carro was successfully destroyed.' }
+      format.html { redirect_to carros_url, notice: 'Carro deletado com sucesso.' }
       format.json { head :no_content }
     end
   end
--- app/models/carro.rb
+++ app/models/carro.rb
@@ -1,4 +1,7 @@
 class Carro < ApplicationRecord
   belongs_to :pessoa
   belongs_to :user
+
+  validates_presence_of :nome, :cor, :placa, :user_id, message: 'deve ser preenchido.'
+  validates_presence_of :pessoa_id, message: 'Esse registro deve ser feito a partir de uma pessoa.'
 end
