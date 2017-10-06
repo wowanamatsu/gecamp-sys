8712b47ae8b489331895d29bda16d5b88f245a0d
wowanamatsu <antonioflavionte@gmail.com>
2017-10-06 17:43:23 -0300

    Ajustes: adicionado exibir para aniversariantes do dia.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @pessoas = Pessoa
-               .select(:nome, :data_nascimento, :celular)
+               .select(:id, :nome, :data_nascimento, :celular)
                .where("EXTRACT(DAY FROM data_nascimento) = #{Time.now.strftime('%d').to_i}")
                .where("EXTRACT(MONTH FROM data_nascimento) = #{Time.now.strftime('%m').to_i}")
                .where(:ativo => 'ativo')
